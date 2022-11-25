6ea3f48f0ad9803bec98954f61b27761e0c751dd
alex <emal@damain.com>
2022-11-25 22:36:00 +0100

    feat: send index

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const express = require ('express');
 const chalk = require ('chalk');
 const debug = require('debug')('app');
-const morgan = require ('morgan')
+const morgan = require ('morgan');
+const path = require ('path')
 
 const app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res)=> {
     res.send('Hi from server')
 });
 
 app.listen(3000, ()=> {
-    // console.log('listening on port 3000');
     debug('listening on port '+ chalk.green('3000'));
 });
