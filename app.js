1b9c271f3e21c92e666be0b34ac8b588dee1a4fd
alex <emal@damain.com>
2022-11-25 22:23:15 +0100

    feat: add convenience  to server

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const express = require ('express')
+const express = require ('express');
+const chalk = require ('chalk');
+const debug = require('debug')('app');
+const morgan = require ('morgan')
 
-const app = express()
+const app = express();
 
+app.use(morgan('tiny'));
 
 app.get('/', (req, res)=> {
     res.send('Hi from server')
-})
+});
 
 app.listen(3000, ()=> {
-    console.log('listening on port 3000');
-})
+    // console.log('listening on port 3000');
+    debug('listening on port '+ chalk.green('3000'));
+});
