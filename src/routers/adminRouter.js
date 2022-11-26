c196091cd174b50076a48f3064f908dfd97d13dc
alex <emal@damain.com>
2022-11-26 22:48:09 +0100

    feat: add one session from db

--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -1,32 +1,27 @@
-const express = require("express");
-const { MongoClient } = require("mongodb");
+const express = require("express");const { MongoClient } = require("mongodb");
 const adminRouter = express.Router();
 const sessionsData = require("../../data/sessions.json");
 const debug = require("debug")("app:adminRouter");
-const {MONGO_CONNECTION_STRING} = require('../../config/config')
-
+const { MONGO_CONNECTION_STRING } = require("../../config/config");
 
 adminRouter.route("/").get((req, res) => {
   const url = MONGO_CONNECTION_STRING;
   const dbName = "nodeapp";
   const collection = "sessions";
-  
+
   (async function mongo() {
     let client;
     try {
-        client =await MongoClient.connect(url)
-        debug('connected to mongo');
-        const db = client.db(dbName)
-        const response = await db.collection(collection).insertMany(sessionsData)
-        res.json(response)
+      client = await MongoClient.connect(url);
+      debug("connected to mongo");
+      const db = client.db(dbName);
+      const response = await db.collection(collection).insertMany(sessionsData);
+      res.json(response);
     } catch (error) {
-        debug(error.stack)
+      debug(error.stack);
     }
-    
-  })()
-
+    client.close();
+  })();
 });
-
-
 
 module.exports = adminRouter;
--- src/routers/sessionRouter.js
+++ src/routers/sessionRouter.js
@@ -1,14 +1,55 @@
 const express = require("express");
 const sessionRouter = express.Router();
-const sessions = require("../../data/sessions.json");
+const { MongoClient, ObjectId } = require("mongodb");
+const debug = require("debug")("app:sessionRouter");
+const { MONGO_CONNECTION_STRING } = require("../../config/config");
 
-sessionRouter.route("/").get((req, res) => {
-    res.render("sessions", { sessions });
-  });
+
   
+//   sessionRouter.route("/:id").get((req, res) => {
+//     const id = req.params.id;
+//     res.render('single', {session: sessions[id]});
+//   });
+
+  sessionRouter.route("/").get((req, res) => {
+    const url = MONGO_CONNECTION_STRING;
+    const dbName = "nodeapp";
+    const collection = "sessions";
+  
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug("connected to mongo");
+        const db = client.db(dbName);
+        const sessions = await db.collection(collection).find().toArray();
+       res.render("sessions", { sessions });
+      } catch (error) {
+        debug(error.stack);
+      }
+       client.close();
+    })();
+  });
+
   sessionRouter.route("/:id").get((req, res) => {
     const id = req.params.id;
-    res.render('single', {session: sessions[id]});
+    const url = MONGO_CONNECTION_STRING;
+    const dbName = "nodeapp";
+    const collection = "sessions";
+  
+    (async function mongo() {
+      let client;
+      try {
+        client = await MongoClient.connect(url);
+        debug("connected to mongo");
+        const db = client.db(dbName);
+        const session = await db.collection(collection).findOne({_id: new ObjectId(id)});
+       res.render("single", { session });
+      } catch (error) {
+        debug(error.stack);
+      }
+       client.close();
+    })();
   });
 
   module.exports = sessionRouter
